a058921746f8667f122aed3daaea0d76d03932fb
Ninter6 <mc525740@outlook.com>
2023-05-20 14:26:47 +0800

    Initial Commit

---
+++ Concraft/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Concraft
+//
+//  Created by Ninter6 on 2023/5/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
